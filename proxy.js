1e96f1412aaeb467e015fb380b54b959666a1785
Bilal sheikh <bilalsheikhsb77@gmail.com>
2026-07-31 22:39:31 +0500

    done

---
+++ proxy.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+export function proxy(NextRequest) {
+    const token = NextRequest.cookies.get("token")?.value;
+    const data = jwt.verify(token, process.env.JWT_SECRET)
+    const requestHeaders = new Headers(NextRequest.headers);
+    console.log(data?.userId,requestHeaders);
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/dashboard/:path*",
+    ],
+};
